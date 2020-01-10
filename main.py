7603c25284824f0852023730099af9b045d03959
Muhammad Izzatul Haq <41615982+haqizza@users.noreply.github.com>
2020-01-10 18:38:10 +0700

    [Clear] 'Simpanan' Feature

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def clear():
 #Fungsi untuk pengaturan pertama kali
 def setting():
     clear()
-    print("=====[First Time Setting]=====")
+    print("=====[ First Time Setting ]=====")
     saldo = int(input("Saldo Awal: Rp."))
     anggaran = int(input("Anggaran Bulanan: RP."))
     simpanan = int(input("Simpan Tabungan: RP."))
@@ -42,12 +42,14 @@ def setting():
     config['set'] = True # Set bahwa sudah diatur
     with open(path+'\config.json', 'w') as outfile:
         json.dump(config,outfile,indent=4)
-    time.sleep(1) # Delay 1 sec
+
+    print("[Setting Selesai]")
+    time.sleep(2) # Delay 2 sec
     main()
 
 def tambahSaldo(now,saldo):
     clear()
-    print("=====[Tambah Saldo]=====")
+    print("=====[ Tambah Saldo ]=====")
     jumlah = int(input("Jumlah: RP."))
     saldo = saldo + jumlah
     
@@ -55,13 +57,66 @@ def tambahSaldo(now,saldo):
     with open(path+'\deposit.json', 'w') as outfile:
         json.dump(dataDeposit,outfile,indent=4)
     
-    print("[Data Ditambahkan]")
+    print("[Saldo Ditambahkan]")
     time.sleep(1)
     main()
 
+def tambahSimpanan(now,simpanan):
+    clear()
+    print("=====[ Tambah Simpanan ]=====")
+    jumlah = int(input("Jumlah: RP."))
+    simpanan = simpanan + jumlah
+    
+    dataDeposit['simpanan'] = simpanan
+    with open(path+'\deposit.json', 'w') as outfile:
+        json.dump(dataDeposit,outfile,indent=4)
+    
+    print("[Simpanan Ditambahkan]")
+    time.sleep(2)
+    simpanan()
+
+def ambilSimpanan(now,simpanan):
+    clear()
+    print("=====[ Ambil Simpanan ]=====")
+    jumlah = int(input("Jumlah: RP."))
+    simpanan = simpanan - jumlah
+    
+    dataDeposit['simpanan'] = simpanan
+    with open(path+'\deposit.json', 'w') as outfile:
+        json.dump(dataDeposit,outfile,indent=4)
+    
+    print("[Simpanan Diambil]")
+    time.sleep(2)
+    simpanan()
+
+def simpanan(now,simpanan):
+    clear()
+    simpanan = dataDeposit["simpanan"]
+    print("=============[ Simpanan ]==============")
+    print("|1. Tambah  ||2. Ambil     |          |")
+    print("|3. Kembali ||4. Menu Utama||5. Keluar|")
+    print("=======================================")
+    print("Simpanan Anda: {}".format(simpanan))
+    
+    pilih = int(input("Menu: "))
+    if(pilih == 1):
+        tambahSimpanan(now,simpanan)
+    elif(pilih == 2):
+        ambilSimpanan(now,simpanan)
+    elif(pilih == 3):
+        pengaturan()
+    elif(pilih == 4):
+        main()
+    elif(pilih == 5):
+        exit()
+    else:
+        print("[Menu Tidak Tersedia]")
+        time.sleep(2)
+        pengaturan()
+
 def transaksi(now,saldo,anggaran):
     clear()
-    print("=====[Transaksi]=====")
+    print("=====[ Transaksi ]=====")
     tujuan = input("Tujuan: ")
     jumlah = int(input("Jumlah: RP."))
     tanggal = now
@@ -95,23 +150,26 @@ def transaksi(now,saldo,anggaran):
         with open(path+'\deposit.json', 'w') as outfile:
             json.dump(dataDeposit,outfile,indent=4)
         
-        print("[Data Ditambahkan]")
+        print("[Transaksi Ditambahkan]")
         time.sleep(1)
         main()
 
-
 def pengaturan():
     clear()
+    saldo = dataDeposit["saldo"]
+    anggaran = dataDeposit["anggaran"]
+    simpanan = dataDeposit["simpanan"]
+
     print("========================[ MENU ]========================")
-    print("|1. Atur Anggaran||2. Simpanan||3. Riwayat||4. Laporan|")
-    print("|5. Kembali      ||6. Keluar  |                       |")
+    print("|1. Simpanan||2. Atur Anggaran||3. Riwayat||4. Laporan|")
+    print("|5. Kembali ||6. Keluar       |                       |")
     print("========================================================")
     pilih = int(input("Menu: "))
 
     if(pilih == 1):
-        aturAnggaran()
+        simpanan(now)
     elif(pilih == 2):
-        simpanan()
+        aturAnggaran(now,anggaran)
     elif(pilih == 3):
         riwayat()
     elif(pilih == 4):
@@ -122,6 +180,7 @@ def pengaturan():
         exit()
     else:
         print("[Menu Tidak Tersedia]")
+        time.sleep(2)
         pengaturan()
     
 def main():
@@ -130,9 +189,10 @@ def main():
     anggaran = dataDeposit["anggaran"]
     simpanan = dataDeposit["simpanan"]
 
-    print("================[ MENU ]================")
-    print("|1. Transaksi||2. Tambah Saldo||3. Pengaturan||4. Keluar|")
-    print("========================================")
+    print("===================[ MENU ]===================")
+    print("|1. Transaksi||2. Tambah Saldo||3. Pengaturan|")
+    print("|4. Keluar   |                               |")
+    print("==============================================")
     print("Saldo Anda: {}".format(saldo))
     print("Anggaran Bulan Ini: {}".format(anggaran))
     print("Simpanan Anda: {}".format(simpanan))
@@ -148,6 +208,7 @@ def main():
         exit()
     else:
         print("[Menu Tidak Tersedia]")
+        time.sleep(2)
         main()
 
 # Eksekusi Pertama
